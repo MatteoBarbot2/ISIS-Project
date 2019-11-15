b5df67ec81fdfa2c8b510a8112c273016e954cac
MatteoBarbot2 <57804265+MatteoBarbot2@users.noreply.github.com>
2019-11-15 17:56:13 +0100

    fonction licence

--- ffssm_enonce/src/main/java/FFSSM/Moniteur.java
+++ ffssm_enonce/src/main/java/FFSSM/Moniteur.java
@@ -8,7 +8,7 @@ import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
-public class Moniteur extends Personne {
+public class Moniteur extends Plongeur {
 
     public int numeroDiplome;
 
--- ffssm_enonce/src/main/java/FFSSM/Plongeur.java
+++ ffssm_enonce/src/main/java/FFSSM/Plongeur.java
@@ -6,9 +6,12 @@ public class Plongeur extends Personne {
 	
     public GroupeSanguin sang;
     public int niveau;
+    public Licence licence;
 
     public Plongeur(String numeroINSEE, String nom, String prenom, String adresse, String telephone, Calendar naissance) {
         super(numeroINSEE, nom, prenom, adresse, telephone, naissance);
     }
-    
+    public void ajouteLicence(Licence L){
+        this.licence = L;
+    }
 }
