3566cf5b2549802cf62c5860d68a30ca0f40c27c
MatteoBarbot2 <57804265+MatteoBarbot2@users.noreply.github.com>
2019-11-15 17:52:39 +0100

    Classe Plongeur

--- ffssm_enonce/src/main/java/FFSSM/Plongeur.java
+++ ffssm_enonce/src/main/java/FFSSM/Plongeur.java
@@ -1,5 +1,14 @@
 package FFSSM;
 
-public class Plongeur {
+import java.util.Calendar;
+
+public class Plongeur extends Personne {
 	
+    public GroupeSanguin sang;
+    public int niveau;
+
+    public Plongeur(String numeroINSEE, String nom, String prenom, String adresse, String telephone, Calendar naissance) {
+        super(numeroINSEE, nom, prenom, adresse, telephone, naissance);
+    }
+    
 }
